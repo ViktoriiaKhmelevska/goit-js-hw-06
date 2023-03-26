9159dccbe7ee747f20023f3d730bbd3fbe84cdd2
Viktoria Khmelevska <vikahvl@ukr.net>
2023-03-26 18:22:05 +0300

    add task5

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -9,3 +9,24 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0;
+const buttonDec = document.querySelector('button[data-action="decrement"]');
+const buttonInc = document.querySelector('button[data-action="increment"]');
+
+let valueEl = document.querySelector("#value");
+
+
+buttonDec.addEventListener("click", onClickDec);
+buttonInc.addEventListener("click", onClickInc);
+
+function onClickDec() {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+    // console.log(valueEl.textContent);
+}
+
+function onClickInc() {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+}
--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,3 +1,30 @@
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
 {/* <input type="text" id="name-input" placeholder="Please enter your name" />
 <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */}
+
+const inputEl = document.querySelector(`#name-input`);
+const outputEl = document.querySelector(`#name-output`);
+// console.dir(inputEl);
+inputEl.addEventListener('change', onInput)
+// inputEl.addEventListener('change', onChange)
+
+function onInput(event) {
+    event.preventDefault();
+    if (!inputEl.value) {
+        console.log(inputEl.value);
+         outputEl.textContent = inputEl.value;
+        // outputEl.value = 'Anonymous';
+        return;
+    } else {
+        // console.log(inputEl.value);
+        outputEl.textContent = inputEl.value;
+    };
+}
+//     // console.log(evt);
+//     console.log(inputEl.value);
+//     // log()
+// }
+
+// function onChange(evt){
+//     alert('Напишіть ще щось')
+// }
