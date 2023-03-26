06567ee96b84535b5879e0af1de06a37ff2fa202
Viktoria Khmelevska <vikahvl@ukr.net>
2023-03-26 23:36:43 +0300

    add all 9tasks

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -17,3 +17,24 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const formEl = document.querySelector(`.login-form`);
+
+formEl.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (password.value === "" || email.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+    const data = {
+    Email: email.value,  
+    Password : password.value
+  }
+  console.log(data);
+  event.currentTarget.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -7,8 +7,19 @@
 
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const buttonEl = document.querySelector(`.change-color`);
+const spanEl = document.querySelector(`.color`);
+const bodyEl = document.querySelector(`body`)
+buttonEl.addEventListener(`click`, changeColor);
+
+function changeColor() {
+    let colorRandon = getRandomHexColor();
+    spanEl.textContent = colorRandon;
+bodyEl.style.backgroundColor = colorRandon;
+}
