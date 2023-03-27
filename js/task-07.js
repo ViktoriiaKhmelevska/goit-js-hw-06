9db9544a8f3bb5c1fa40965edef8d822e89fed41
Viktoria Khmelevska <vikahvl@ukr.net>
2023-03-28 00:03:25 +0300

    corrected task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,6 @@
 const inputEl = document.querySelector(`#font-size-control`);
 const spanEl = document.querySelector(`#text`)
-inputEl = addEventListener(`input`, onClick);
+inputEl.addEventListener(`input`, onClick);
 function onClick() {
-    for (let i = 0; i < Number(inputEl.value); i += 1) { 
-    spanEl.style.fontSize = `${inputEl.value}px`;
-};
+      spanEl.style.fontSize = `${inputEl.value}px`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,8 +1,3 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
 // Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
 
 // <div id="controls">
@@ -18,10 +13,15 @@
 // Розміри найпершого <div> - 30px на 30px.
 // Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
 // Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
 
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+const divEl = document.querySelector(`#boxes`)
+
+function destroyBoxes() {
+
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
